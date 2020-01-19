428cd74bea07d985a9dd7bad1cf4676ae351438c
Alex Simpson <alexander.simpson@target.com>
2020-01-19 16:32:37 -0600

    initial commit with exercise 1

---
+++ src/com/exercises/week1/Exercise1.java
@@ -0,0 +1,35 @@
+package com.exercises.week1;
+import java.util.Scanner;
+
+public class Exercise1 {
+    public static void main(String[]args) {
+        Scanner Input = new Scanner(System.in);
+        // 1: First, the white space is different when defining the variables
+        // Second, the defined integer VAL1 should be lowercase as this is closer to a constant naming convention
+
+        // 2:
+        System.out.println("Integer 1:");
+        int userInputInt1 = Input.nextInt();
+        System.out.println("Integer 2:");
+        int userInputInt2 = Input.nextInt();
+        System.out.println("Integer 3:");
+        int userInputInt3 = Input.nextInt();
+
+        int multipliedIntegers = userInputInt1 * userInputInt2;
+        int finalResult = multipliedIntegers - userInputInt3;
+
+        System.out.println("The final result is: " + finalResult);
+
+        // 3:
+        // get user input 1 and set to input1
+        // get user input 2 and set to input2
+        // get user input 3 and set to input3
+        // if input1 > input2
+            // if input1 > input3
+                // return input1
+        // else if input2 > input3
+            // return input2
+        // else
+            // return input3
+    }
+}
