2264fbe70b4e430044101dc013de229b34cd9ad6
Alex Simpson <alexander.simpson@target.com>
2020-01-22 17:53:03 -0600

    updated input

--- src/com/labs/week1/Lab1.java
+++ src/com/labs/week1/Lab1.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 
 public class Lab1 {
     public static void main(String[] args) {
-        Scanner Input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         // simple program to add and average users input
         System.out.print("Enter in your first number: ");
-        double userInput1 = Input.nextInt();
+        double userInput1 = input.nextInt();
         System.out.print("Enter in your second number: ");
-        double userInput2 = Input.nextInt();
+        double userInput2 = input.nextInt();
         System.out.print("Enter in your last number: ");
-        double userInput3 = Input.nextInt();
+        double userInput3 = input.nextInt();
 
         // quick math
         double sumOfUserInputs = userInput1 + userInput2 + userInput3;
