934dd9f99474b16b9060670df12c44463b7a7a8b
Alex Simpson <alexander.simpson@target.com>
2020-01-29 19:44:26 -0600

    overrode multiplyValues by changing the signiture

--- src/com/exercises/week3/Exercise3.java
+++ src/com/exercises/week3/Exercise3.java
@@ -3,10 +3,15 @@ package com.exercises.week3;
 public class Exercise3 {
     public static void main(String[] args) {
         System.out.println(multiplyValues(1, 5));
+        System.out.println(multiplyValues(1, 5, 8));
 
     }
 
     public static double multiplyValues(double val1, double val2) {
         return val1 * val2;
+    }
+
+    public static double multiplyValues(double val1, double val2, double val3) {
+        return val1 * val2 * val3;
     }
 }
