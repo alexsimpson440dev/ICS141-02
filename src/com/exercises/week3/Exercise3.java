322582b3404fd8757fdfe7b889ab29039a3652a0
Alex Simpson <alexander.simpson@target.com>
2020-01-29 19:04:38 -0600

    added exercise 3

---
+++ src/com/exercises/week3/Exercise3.java
@@ -0,0 +1,12 @@
+package com.exercises.week3;
+
+public class Exercise3 {
+    public static void main(String[] args) {
+        System.out.println(multiplyValues(1, 5));
+
+    }
+
+    public static double multiplyValues(double val1, double val2) {
+        return val1 * val2;
+    }
+}
