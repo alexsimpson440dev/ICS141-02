d532807db962e35ca7a5660fa44dbad873e1d736
Alex Simpson <alexander.simpson@target.com>
2020-02-03 23:50:50 -0600

    added variable

--- src/com/labs/week3/Lab3.java
+++ src/com/labs/week3/Lab3.java
@@ -2,7 +2,7 @@ package com.labs.week3;
 
 public class Lab3 {
     public static void main(String[] args) {
-        printMax(2, 2, 10);
+        int max = printMax(2, 2, 2);
     }
 
     public static int printMax(int num1, int num2, int printValue) {
